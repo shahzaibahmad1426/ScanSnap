9ce971b06527443b3a92065674e70e647563e0e8
Shahzaib Ahmad <shahzaibahmadfd01@gmail.com>
2024-06-10 13:20:12 +0500

    Building QR Code (Scanner and Generator) Widgets

---
+++ lib/qr_code_generator.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+
+class QrCodeGenerator extends StatefulWidget {
+  const QrCodeGenerator({super.key});
+
+  @override
+  State<QrCodeGenerator> createState() => _QrCodeGeneratorState();
+}
+
+class _QrCodeGeneratorState extends State<QrCodeGenerator> {
+  TextEditingController urlController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: const Text(
+          'ScanSnap',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (urlController.text.isNotEmpty)
+                  QrImageView(data : urlController.text, size : 200),
+                const SizedBox(
+                  height: 10,
+                ),
+              Container(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: TextField(
+                  controller: urlController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter the data',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelText: 'Enter the data',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10,),
+              ElevatedButton(onPressed: (){setState(() {});}, child: const Text('Generate QR Code'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
